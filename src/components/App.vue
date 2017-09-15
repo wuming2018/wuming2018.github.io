e98e6ec680153273a175e12ac1aaa708adede65e
kai2017_qq.com_wuming <kai2017@qq.com>
2017-09-15 13:57:23 +0800

    test

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app" class="docs scotchified">
+    <nav class="main">
+        <div class="container">
+
+            <router-link :to="'/list'" class="brand">
+                青枫浦 Lite</router-link>
+        </div>
+    </nav>
+    <section class="section" id="top">
+        <router-view>
+        </router-view>
+    </section>
+    <footer class="main">
+        <p>This is A simple blog light style.</p>
+        <p class="less-significant">say thanks to <a href="https://github.com/rozbo">Github</a></p>
+    </footer>
+</div>
+</template>
+<script>
+import * as css from '../style/main.css'
+export default {
+    name: 'app',
+    data: function() {
+        return {
+            subtitle: '青枫浦 Lite',
+        }
+    }
+}
+</script>
