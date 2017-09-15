06c12ed92cae5c7f3350310183825e4bfc81b953
kai2017_qq.com_wuming <kai2017@qq.com>
2017-09-15 16:02:15 +0800

    test

---
+++ src/helpers/github.js
@@ -0,0 +1,45 @@
+import {
+    EventEmitter
+} from 'events'
+import Vue from 'vue'
+const github = new EventEmitter()
+const itemsCache = Object.create(null);
+const githubURl = 'https://api.github.com/repos/wuming2018/wuming2018.github.io';
+const author='wuming2018';
+let listCache='';
+
+github.getList = function() {
+    return new Promise((resolve, reject) => {
+        if(listCache!=''){
+            resolve(listCache);
+        }
+        Vue.http.get(githubURl+'?creator='+author).then(
+            (response) => {
+                listCache=response.body;
+                resolve(response.body);
+            }, reject
+        );
+    });
+}
+
+github.getDetail = id => {
+    return new Promise((resolve, reject) => {
+        if (itemsCache[id]) {
+            console.log("bycache");
+            resolve(itemsCache[id])
+        } else {
+            Vue.http.get(githubURl + '/' + id).then(
+                (response) => {
+                    console.log(response);
+                    itemsCache[id] = response.body;
+                    console.log("by get");
+                    resolve(itemsCache[id]);
+                }, reject
+            );
+        }
+    });
+}
+
+export {
+    github
+};
